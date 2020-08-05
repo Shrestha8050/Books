fb995758798a668ae6c0f27619db4debbf0f3a2f
Bodhan Shrestha <bodhanshrestha@gmail.com>
2020-08-05 16:00:42 +0545

    First Commmit

---
+++ server/config/Config.js
@@ -0,0 +1,16 @@
+const config = {
+    production: {
+        SECRET: process.env.SECRET,
+        DATABASE: process.env.MONGODB_URI
+    },
+    default: {
+        SECRET: 'helloworld',
+        DATABASE: 'mongodb://localhost:27017/bookShelf'
+    }
+
+
+}
+
+exports.get = function get(env) {
+    return config[env] || config.default
+}
